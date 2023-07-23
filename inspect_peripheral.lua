69c9fb3f71db0d690fcaa028a5692f3260329f1b
Joris Schelfhout <joris@shelfwood.co>
2023-07-23 14:51:16 +0200

    Add inspect_peripheral package

---
+++ inspect_peripheral.lua
@@ -0,0 +1,31 @@
+-- Function to inspect a peripheral and print out its methods
+function inspectPeripheral(side)
+    -- Check if there is a peripheral connected on the given side
+    if not peripheral.isPresent(side) then
+      print("No peripheral present on the " .. side .. " side.")
+      return
+    end
+    
+    -- Get the methods of the peripheral
+    local methods = peripheral.getMethods(side)
+  
+    -- If there are no methods available, print a message and return
+    if methods == nil or #methods == 0 then
+      print("No methods available for the peripheral on the " .. side .. " side.")
+      return
+    end
+  
+    -- Print the methods
+    print("Methods for the peripheral on the " .. side .. " side:")
+    for _, method in ipairs(methods) do
+      print(method)
+    end
+  end
+  
+  -- Ask the user to enter the side
+  print("Enter the side where the peripheral is connected (top, bottom, left, right, front or back):")
+  local side = read()
+  
+  -- Call the function to inspect the peripheral
+  inspectPeripheral(side)
+  
