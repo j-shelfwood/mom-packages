395c84968dd57f59fb97b25797b56db1fc2f3acd
Joris Schelfhout <joris@shelfwood.co>
2023-07-23 20:51:45 +0200

    Change display strat

--- item_monitor.lua
+++ item_monitor.lua
@@ -1,9 +1,3 @@
--- Constants for the number of items per page
-local ITEMS_PER_PAGE = 30
-
--- Constants for the number of columns
-local COLUMNS = 2
-
 -- Function to check if a table contains a specific value
 function tableContains(table, element)
   for _, value in pairs(table) do
@@ -25,8 +19,8 @@ function sortItems(items)
   table.sort(items, function(a, b) return a.count > b.count end)
 end
 
--- Function to get and display items from a peripheral on a monitor
-function displayItems(monitorSide, peripheralSide)
+-- Function to display item information in a "powerpoint" style
+function displayItemInfo(monitorSide, peripheralSide)
   -- Validate the sides
   if not validateSide(peripheralSide) then
     print("Invalid side: " .. peripheralSide)
@@ -62,66 +56,31 @@ function displayItems(monitorSide, peripheralSide)
 
   local width, height = monitor.getSize()
 
-  -- Initialize the current page
-  local currentPage = 1
-
   -- Continuously fetch and display the items
   while true do
     -- Update items
     local items = interface.items()
     sortItems(items)
 
-    -- Determine the number of pages
-    local numPages = math.ceil(#items / ITEMS_PER_PAGE)
-
-    -- Ensure the current page is valid
-    currentPage = math.min(currentPage, numPages)
-    currentPage = math.max(currentPage, 1)
-
-    -- Determine the range of items to display on this page
-    local startItem = (currentPage - 1) * ITEMS_PER_PAGE + 1
-    local endItem = math.min(startItem + ITEMS_PER_PAGE - 1, #items)
-
-    -- Clear the monitor
-    monitor.clear()
-    monitor.setCursorPos(1, 1)
-
-    -- Write the page number and total number of pages to the monitor
-    monitor.write("Page " .. tostring(currentPage) .. " of " .. tostring(numPages))
-
-    -- Write the items to the monitor
-    local line = 2
-    for i = startItem, endItem do
-      -- Check if we've reached the maximum number of lines
-      if line > height then
-        break
-      end
-      
-      -- Write the item to the monitor
+    for i = 1, math.min(#items, 50) do
       local item = items[i]
-      local itemDisplay = string.format("%s %s", item.name, tostring(item.count))
+      local itemName = item.name
+      local itemCount = item.count
       
-      -- Determine the column to write to
-      local column = (i - startItem) % COLUMNS
-      local columnWidth = math.floor(width / COLUMNS)
+      -- Clear the monitor
+      monitor.clear()
       
-      -- Right-align the item count within the column
-      monitor.setCursorPos(column * columnWidth + 1, line)
-      monitor.write(itemDisplay)
-
-      -- Increment the line number
-      if column == COLUMNS - 1 then
-        line = line + 1
-      end
-    end
-
-    -- Wait for the user to change the page or for the items to be updated
-    local event, key = os.pullEventTimeout(1)
-    if event == "key" then
-      if key == keys.right then
-        currentPage = currentPage + 1
-      elseif key == keys.left then
-        currentPage = currentPage - 1
+      -- Write the item name and count
+      monitor.setCursorPos(math.floor((width - #itemName) / 2) + 1, math.floor(height / 2))
+      monitor.write(itemName)
+      monitor.setCursorPos(math.floor((width - #tostring(itemCount)) / 2) + 1, math.floor(height / 2) + 1)
+      monitor.write(tostring(itemCount))
+      
+      -- Write the "dots"
+      for dot = 3, 1, -1 do
+        monitor.setCursorPos(width - dot, height)
+        monitor.write(".")
+        sleep(1)
       end
     end
   end
@@ -134,5 +93,5 @@ local peripheralSide = read()
 print("Enter the side where the monitor is connected (top, bottom, left, right, front or back):")
 local monitorSide = read()
 
--- Call the function to display the items
-displayItems(monitorSide, peripheralSide)
+-- Call the function to display the item information
+displayItemInfo(monitorSide, peripheralSide)
