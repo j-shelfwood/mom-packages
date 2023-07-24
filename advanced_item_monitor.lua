621d89d3c049f39328b3952df611395ccb2b3005
Joris Schelfhout <joris@shelfwood.co>
2023-07-25 00:39:00 +0200

    GPT

--- advanced_item_monitor.lua
+++ advanced_item_monitor.lua
@@ -19,42 +19,30 @@ function findPeripheralSide(name)
   return nil
 end
 
--- Function to split text into lines of at most a given length
-function wordWrap(text, length)
-  local lines = {}
-  local line = ""
-  for word in text:gmatch("%S+") do
-    if #line + #word <= length then
-      line = line .. (line == "" and "" or " ") .. word
-    else
-      table.insert(lines, line)
-      line = word
-    end
-  end
-  if line ~= "" then
-    table.insert(lines, line)
-  end
-  return lines
+-- Function to write centered text in a cell
+function writeCentered(monitor, row, col, cellWidth, cellHeight, text, line)
+  local x = (col - 1) * cellWidth + math.floor((cellWidth - #text) / 2) + 1
+  local y = (row - 1) * cellHeight + line
+  monitor.setCursorPos(x, y)
+  monitor.write(text)
 end
 
--- Function to write centered text in a cell
-function writeCentered(monitor, row, col, cellWidth, cellHeight, textLines, lineStart)
-  for i, text in ipairs(textLines) do
-    local x = (col - 1) * cellWidth + math.floor((cellWidth - #text) / 2) + 1
-    local y = (row - 1) * cellHeight + lineStart + i - 1
-    monitor.setCursorPos(x, y)
-    monitor.write(text)
+-- Function to shorten item names if they're too long
+function shortenName(name)
+  if #name <= 18 then
+    return name
+  elseif #name > 30 then
+    return name:sub(1, 10) .. "..." .. name:sub(-10, -1)
+  else
+    return name:sub(1, 18) .. "..."
   end
 end
 
 -- Function to display item information in a grid
-function displayItemInfo(monitorSide, peripheralSide, numColumns, numRows, textScale)
+function displayItemInfo(monitorSide, peripheralSide, numColumns, numRows)
   -- Get a reference to the monitor and the peripheral
   local monitor = peripheral.wrap(monitorSide)
   local interface = peripheral.wrap(peripheralSide)
-
-  -- Set the text scale
-  monitor.setTextScale(textScale)
 
   -- Get monitor dimensions and calculate cell dimensions
   local monitorWidth, monitorHeight = monitor.getSize()
@@ -66,7 +54,6 @@ function displayItemInfo(monitorSide, peripheralSide, numColumns, numRows, textS
 
   -- Continuously fetch and display the items
   while true do
-    print("Starting new iteration...")
     -- Clear the monitor
     monitor.clear()
 
@@ -81,32 +68,31 @@ function displayItemInfo(monitorSide, peripheralSide, numColumns, numRows, textS
       local row = math.floor((i - 1) / numColumns) + 1
       local col = (i - 1) % numColumns + 1
       local item = items[i]
-      -- local itemNameLines = wordWrap(item.name, cellWidth)
-      local itemNameLines = {item.name}
+      local itemName = shortenName(item.name)
       local itemCount = item.count
       local itemChange = ""
 
       -- Calculate the change from the previous count
-      if prevItems[item.name] then
-        local change = itemCount - prevItems[item.name].count
+      if prevItems[itemName] then
+        local change = itemCount - prevItems[itemName].count
         if change > 0 then
           itemChange = "+"
-          prevItems[item.name].noChangeCount = 0
+          prevItems[itemName].noChangeCount = 0
         elseif change < 0 then
           itemChange = "-"
-          prevItems[item.name].noChangeCount = 0
-        elseif prevItems[item.name].noChangeCount < 10 then
-          itemChange = prevItems[item.name].change
-          prevItems[item.name].noChangeCount = prevItems[item.name].noChangeCount + 1
+          prevItems[itemName].noChangeCount = 0
+        elseif prevItems[itemName].noChangeCount < 10 then
+          itemChange = prevItems[itemName].change
+          prevItems[itemName].noChangeCount = prevItems[itemName].noChangeCount + 1
         end
       end
 
       -- Save the current count and change for the next update
-      prevItems[item.name] = {count = itemCount, change = itemChange, noChangeCount = (prevItems[item.name] and prevItems[item.name].noChangeCount or 0)}
-      print("Displaying item:", item.name, itemCount, itemChange)
+      prevItems[itemName] = {count = itemCount, change = itemChange, noChangeCount = (prevItems[itemName] and prevItems[itemName].noChangeCount or 0)}
+
       -- Write the item name, count and change in their respective cell
-      writeCentered(monitor, row, col, cellWidth, cellHeight, itemNameLines, 1)
-      writeCentered(monitor, row, col, cellWidth, cellHeight, {tostring(itemCount) .. " " .. itemChange}, #itemNameLines + 1)
+      writeCentered(monitor, row, col, cellWidth, cellHeight, itemName, 1)
+      writeCentered(monitor, row, col, cellWidth, cellHeight, tostring(itemCount) .. " " .. itemChange, 2)
     end
   end
 end
@@ -132,9 +118,5 @@ local numColumns = tonumber(read())
 print("Enter the number of rows for the item grid:")
 local numRows = tonumber(read())
 
--- Ask for the text scale
-print("Enter the text scale (0.5 - 5):")
-local textScale = tonumber(read()) or 0.5
-
 -- Call the function to display the item information
-displayItemInfo(monitorSide, peripheralSide, numColumns, numRows, textScale)
+displayItemInfo(monitorSide, peripheralSide, numColumns, numRows)
