c909d861fc80f38a1cd331972de2d8d6d06cb4db
Joris Schelfhout <joris@shelfwood.co>
2023-07-24 21:24:15 +0200

    Wrong change

--- advanced_item_monitor.lua
+++ advanced_item_monitor.lua
@@ -1,100 +1,93 @@
-local ae2_side = ""
-local monitor_side = ""
-local sides = {"left", "right", "front", "back", "top", "bottom"}
-
--- Helper function to clear monitor
-function clearMonitor(monitor)
-  monitor.clear()
-  monitor.setCursorPos(1,1)
-end
-
 -- Function to check if a table contains a specific value
 function tableContains(table, element)
-  for _, value in pairs(table) do
-    if value == element then
-      return true
+    for _, value in pairs(table) do
+      if value == element then
+        return true
+      end
     end
-  end
-  return false
-end
-
--- Detect peripherals
-for _, side in pairs(sides) do
-  if peripheral.getType(side) == "merequester:requester" then
-    ae2_side = side
-  elseif peripheral.getType(side) == "monitor" then
-    monitor_side = side
-  end
-end
-
--- Check for the required peripherals
-if ae2_side == "" then
-  error("No AE2 system detected. Please ensure the computer is connected to an AE2 system.")
-end
-
-if monitor_side == "" then
-  print("No monitor detected. Output will be printed to the terminal.")
-end
-
--- Get AE2 system and monitor peripherals
-local ae2 = peripheral.wrap(ae2_side)
-local monitor = peripheral.wrap(monitor_side)
-clearMonitor(monitor)
-
--- Function to sort items by count
-function sortItems(items)
-  table.sort(items, function(a, b) return a.count > b.count end)
-end
-
-print("Enter the number of items to show:")
-local numItemsToShow = tonumber(read())
-
-print("Enter the number of columns for the display grid:")
-local numColumns = tonumber(read())
-
-local lastCounts = {}
-local items = ae2.listItems()
-sortItems(items)
-
-for i = 1, numItemsToShow do
-  local item = items[i]
-  lastCounts[item.name] = item.count
-end
-
--- Display items in a grid
-while true do
-  clearMonitor(monitor)
-  local items = ae2.listItems()
-  sortItems(items)
-  
-  local rowIndex = 1
-  for i = 1, numItemsToShow do
-    local item = items[i]
-    local itemName = item.name
-    local itemCount = item.count
-    local difference = itemCount - (lastCounts[itemName] or 0)
-    lastCounts[itemName] = itemCount
-    
-    for j = 1, numColumns do
-      monitor.setCursorPos((j - 1) * 12 + 1, rowIndex)
-      monitor.write(itemName)
-      monitor.setCursorPos((j - 1) * 12 + 1, rowIndex + 1)
-      monitor.write("Count: " .. itemCount)
-      monitor.setCursorPos((j - 1) * 12 + 1, rowIndex + 2)
-      monitor.write("Difference: " .. difference)
-      
-      i = i + 1
-      if i > numItemsToShow then break end
-      item = items[i]
-      itemName = item.name
-      itemCount = item.count
-      difference = itemCount - (lastCounts[itemName] or 0)
-      lastCounts[itemName] = itemCount
-    end
-    
-    rowIndex = rowIndex + 3
-    if rowIndex > monitor.getSize() then break end
+    return false
   end
   
-  sleep(10)
-end
+  -- Function to sort items by count
+  function sortItems(items)
+    table.sort(items, function(a, b) return a.count > b.count end)
+  end
+  
+  -- Function to auto-detect peripherals
+  function detectPeripherals()
+    local sides = {"left", "right", "front", "back", "top", "bottom"}
+    local monitorSide, peripheralSide
+  
+    for _, side in pairs(sides) do
+      if peripheral.getType(side) == "monitor" then
+        monitorSide = side
+      elseif peripheral.getType(side) == "meBridge" then
+        peripheralSide = side
+      end
+    end
+    
+    return monitorSide, peripheralSide
+  end
+  
+  -- Function to display item information in a "powerpoint" style
+  function displayItemInfo(monitorSide, peripheralSide, numItems, numColumns)
+    local monitor = peripheral.wrap(monitorSide)
+    local interface = peripheral.wrap(peripheralSide)
+  
+    local width, height = monitor.getSize()
+  
+    local columnWidth = math.floor(width / numColumns)
+    local rows = math.ceil(numItems / numColumns)
+    
+    -- previous state for change calculation
+    local prevItems = {}
+  
+    -- Continuously fetch and display the items
+    while true do
+      local items = interface.getItemsInNetwork()
+      sortItems(items)
+  
+      monitor.clear()
+  
+      for row = 1, rows do
+        for column = 1, numColumns do
+          local index = (row - 1) * numColumns + column
+          if index > numItems then
+            break
+          end
+  
+          local item = items[index]
+          local itemName = item.label
+          local itemCount = item.size
+          local itemChange = (prevItems[itemName] or itemCount) - itemCount
+  
+          -- Save current count for next loop
+          prevItems[itemName] = itemCount
+  
+          -- Print item data to screen
+          monitor.setCursorPos((column - 1) * columnWidth + 1, row)
+          monitor.write(string.format("%s: %d (%+d)", itemName, itemCount, itemChange))
+        end
+      end
+  
+      sleep(10)
+    end
+  end
+  
+  -- Auto-detect peripherals
+  local monitorSide, peripheralSide = detectPeripherals()
+  
+  if not monitorSide or not peripheralSide then
+    error("Failed to detect necessary peripherals. Make sure they are connected.")
+  end
+  
+  -- Ask the user for the display parameters
+  print("Enter the number of items to display:")
+  local numItems = tonumber(read())
+  
+  print("Enter the number of columns for the display grid:")
+  local numColumns = tonumber(read())
+  
+  -- Call the function to display the item information
+  displayItemInfo(monitorSide, peripheralSide, numItems, numColumns)
+  
--- item_monitor.lua
+++ item_monitor.lua
@@ -8,85 +8,90 @@ function tableContains(table, element)
   return false
 end
 
+-- Function to validate side input
+function validateSide(side)
+  local validSides = {"top", "bottom", "left", "right", "front", "back"}
+  return tableContains(validSides, side)
+end
+
 -- Function to sort items by count
 function sortItems(items)
   table.sort(items, function(a, b) return a.count > b.count end)
 end
 
--- Function to auto-detect peripherals
-function detectPeripherals()
-  local sides = {"left", "right", "front", "back", "top", "bottom"}
-  local monitorSide, peripheralSide
-
-  for _, side in pairs(sides) do
-    if peripheral.getType(side) == "monitor" then
-      monitorSide = side
-    elseif peripheral.getType(side) == "meBridge" then
-      peripheralSide = side
-    end
+-- Function to display item information in a "powerpoint" style
+function displayItemInfo(monitorSide, peripheralSide)
+  -- Validate the sides
+  if not validateSide(peripheralSide) then
+    print("Invalid side: " .. peripheralSide)
+    return
   end
   
-  return monitorSide, peripheralSide
-end
+  if not validateSide(monitorSide) then
+    print("Invalid side: " .. monitorSide)
+    return
+  end
 
--- Function to display item information in a "powerpoint" style
-function displayItemInfo(monitorSide, peripheralSide, numItems, numColumns)
+  -- Check if there are peripherals connected on the given sides
+  if not peripheral.isPresent(peripheralSide) then
+    print("No peripheral present on the " .. peripheralSide .. " side.")
+    return
+  end
+  
+  if not peripheral.isPresent(monitorSide) then
+    print("No monitor present on the " .. monitorSide .. " side.")
+    return
+  end
+
+  -- Check if the peripheral has an 'items' method
+  local methods = peripheral.getMethods(peripheralSide)
+  if not tableContains(methods, "items") then
+    print("The peripheral on the " .. peripheralSide .. " side does not have an 'items' method.")
+    return
+  end
+
+  -- Get a reference to the monitor and the peripheral
   local monitor = peripheral.wrap(monitorSide)
   local interface = peripheral.wrap(peripheralSide)
 
   local width, height = monitor.getSize()
 
-  local columnWidth = math.floor(width / numColumns)
-  local rows = math.ceil(numItems / numColumns)
-  
-  -- previous state for change calculation
-  local prevItems = {}
-
   -- Continuously fetch and display the items
   while true do
-    local items = interface.getItemsInNetwork()
+    -- Update items
+    local items = interface.items()
     sortItems(items)
 
-    monitor.clear()
-
-    for row = 1, rows do
-      for column = 1, numColumns do
-        local index = (row - 1) * numColumns + column
-        if index > numItems then
-          break
-        end
-
-        local item = items[index]
-        local itemName = item.label
-        local itemCount = item.size
-        local itemChange = (prevItems[itemName] or itemCount) - itemCount
-
-        -- Save current count for next loop
-        prevItems[itemName] = itemCount
-
-        -- Print item data to screen
-        monitor.setCursorPos((column - 1) * columnWidth + 1, row)
-        monitor.write(string.format("%s: %d (%+d)", itemName, itemCount, itemChange))
+    for i = 1, math.min(#items, 50) do
+      local item = items[i]
+      local itemName = item.name
+      local itemCount = item.count
+      
+      -- Clear the monitor
+      monitor.clear()
+      
+      -- Write the item name and count
+      monitor.setCursorPos(math.floor((width - #itemName) / 2) + 1, math.floor(height / 2))
+      monitor.write(itemName)
+      monitor.setCursorPos(math.floor((width - #tostring(itemCount)) / 2) + 1, math.floor(height / 2) + 1)
+      monitor.write(tostring(itemCount))
+      
+      -- Write the "dots"
+      for dot = 3, 1, -1 do
+        monitor.setCursorPos(width - dot, height)
+        monitor.write(".")
+        sleep(1)
       end
     end
-
-    sleep(10)
   end
 end
 
--- Auto-detect peripherals
-local monitorSide, peripheralSide = detectPeripherals()
+-- Ask the user to enter the sides
+print("Enter the side where the AE2 peripheral is connected (top, bottom, left, right, front or back):")
+local peripheralSide = read()
 
-if not monitorSide or not peripheralSide then
-  error("Failed to detect necessary peripherals. Make sure they are connected.")
-end
-
--- Ask the user for the display parameters
-print("Enter the number of items to display:")
-local numItems = tonumber(read())
-
-print("Enter the number of columns for the display grid:")
-local numColumns = tonumber(read())
+print("Enter the side where the monitor is connected (top, bottom, left, right, front or back):")
+local monitorSide = read()
 
 -- Call the function to display the item information
-displayItemInfo(monitorSide, peripheralSide, numItems, numColumns)
+displayItemInfo(monitorSide, peripheralSide)
