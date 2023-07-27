77e22869b941a0e46c60e819f70c42d328dd297b
Joris Schelfhout <joris@shelfwood.co>
2023-07-27 20:49:40 +0200

    Use cobalt

--- ae2_input_monitor.lua
+++ ae2_input_monitor.lua
@@ -1,101 +1,64 @@
+local cobalt = require("cobalt.cobalt")
 local generics = require("generics")
-local GridMonitor = require("grid_monitor") -- require the GridMonitor module
+local peripheralSide = generics.findPeripheralSide("merequester:requester")
 
-local scale = tonumber(arg[1]) or 1
+-- Define the format for our items.
+local function formatItem(item)
+    return item.name .. "\n" .. item.sign .. " " .. tostring(item.change)
+end
 
--- Function to track input of items
-function trackInput(monitorSide, peripheralSide)
-    -- Get a reference to the monitor and the peripheral
-    local gm = GridMonitor.new(peripheral.wrap(monitorSide), scale) -- use GridMonitor
-    gm:debugInfo() -- print debug information
+-- Define our main loop
+cobalt.loop = function()
+    -- Get our peripheral
     local requester = peripheral.wrap(peripheralSide)
-
-    -- Initialize the previous items table and the changes table
-    local prevItems = {}
+    local items = requester.items()
     local changes = {}
 
-    -- Continuously fetch and display the items
-    while true do
-        -- Get items
-        local items = requester.items()
+    -- Clear out our view
+    cobalt.view.children = {}
 
-        -- Initialize the current items table
-        local currentItems = {}
+    -- Create a row for each item
+    for _, item in ipairs(items) do
+        local itemName = generics.shortenName(item.name, 15)
+        local itemCount = item.count
 
-        for _, item in ipairs(items) do
-            local itemName = generics.shortenName(item.name, 15) -- change the second parameter
-            local itemCount = item.count
-
-            -- Save the current count for calculating the change
-            currentItems[itemName] = itemCount
-
-            -- If the item was already present, calculate the change
-            if prevItems[itemName] then
-                local change = itemCount - prevItems[itemName]
-
-                -- If there was a change, store it
-                if change ~= 0 then
-                    changes[itemName] = {
-                        change = math.abs(change),
-                        sign = change > 0 and "+" or "-"
-                    }
-                else
-                    changes[itemName] = nil
-                end
+        -- If the item was already present, calculate the change
+        if prevItems and prevItems[itemName] then
+            local change = itemCount - prevItems[itemName]
+            -- If there was a change, store it
+            if change ~= 0 then
+                changes[itemName] = {
+                    change = math.abs(change),
+                    sign = change > 0 and "+" or "-"
+                }
+            else
+                changes[itemName] = nil
             end
         end
 
-        -- Update the previous items table
-        prevItems = currentItems
-
-        -- Convert the changes table to a list and sort it by absolute value of change
-        local sortedChanges = {}
-        for itemName, changeData in pairs(changes) do
-            table.insert(sortedChanges, {
+        -- Add our item to the view
+        local column = cobalt.ui.new("Column", {
+            width = 15,
+            height = 3
+        })
+        local label = cobalt.ui.new("Label", {
+            text = formatItem({
                 name = itemName,
-                change = changeData and changeData.change or 0,
-                sign = changeData and changeData.sign or "+"
+                sign = changes[itemName] and changes[itemName].sign or "+",
+                change = changes[itemName] and changes[itemName].change or 0
             })
-        end
-        table.sort(sortedChanges, function(a, b)
-            return a.change > b.change
-        end)
+        })
 
-        -- Keep only the top X changes
-        while #sortedChanges > gm.numColumns * gm.numRows do
-            table.remove(sortedChanges)
-        end
-
-        -- Clear the monitor
-        gm:clearGrid()
-
-        -- If there are no changes, create an empty grid
-        if #sortedChanges == 0 then
-            for i = 1, gm.numColumns do
-                for j = 1, gm.numRows do
-                    gm:displayData({{
-                        name = "...",
-                        sign = "",
-                        change = "0"
-                    }}, function(item)
-                        return item.name .. "\n" .. item.sign .. " " .. tostring(item.change)
-                    end, i, j)
-                end
-            end
-        else
-            -- Display changes in the grid
-            gm:displayData(sortedChanges, function(item)
-                return item.name .. "\n" .. item.sign .. " " .. tostring(item.change)
-            end)
-        end
-
-        sleep(10)
+        column:addChild(label)
+        cobalt.view:addChild(column)
     end
+
+    -- Store the current items for the next iteration
+    prevItems = items
 end
 
 -- Automatically find the sides
 local monitorSide = generics.findPeripheralSide("monitor")
-local peripheralSide = generics.findPeripheralSide("merequester:requester")
 
 if not monitorSide then
     print("Monitor not found.")
@@ -107,5 +70,6 @@ if not peripheralSide then
     return
 end
 
--- Call the function to track the input of items
-trackInput(monitorSide, peripheralSide)
+-- Start Cobalt
+cobalt.init(monitorSide)
+cobalt.run()
