640174a9c0ebcd697e39dc4ce043fb14df204a57
Joris Schelfhout <joris@shelfwood.co>
2023-07-27 21:11:05 +0200

    GPT

--- ae2_input_monitor.lua
+++ ae2_input_monitor.lua
@@ -1,93 +1,64 @@
-local cobalt = dofile("path/to/cobalt") -- replace with the correct path to your Cobalt file
 local generics = require("generics")
-
--- Automatically find the sides
-local monitorSide = generics.findPeripheralSide("monitor")
 local peripheralSide = generics.findPeripheralSide("merequester:requester")
+local monitorSide = generics.findPeripheralSide("monitor")
 
-if not monitorSide then
-    print("Monitor not found.")
-    return
-end
+-- Set up monitor
+local monitor = peripheral.wrap(monitorSide)
+monitor.setTextScale(0.7)
 
-if not peripheralSide then
-    print("ME Requester not found.")
-    return
-end
+-- Dimensions for a 5x7 monitor
+local cellWidth = math.floor(monitor.getWidth() / 7)
+local cellHeight = math.floor(monitor.getHeight() / 5)
 
--- Global variable to store the previous items
-prevItems = {}
+-- Store previous items
+local prevItems = {}
 
--- Helper function to format an item
-function formatItem(item)
-    return item.name .. "\n" .. item.sign .. " " .. tostring(item.change)
-end
+-- Function to track input of items
+while true do
+    -- Get items
+    local items = peripheral.wrap(peripheralSide).items()
+    local changes = {}
 
--- Callback for the update phase
-function cobalt.update(dt)
-    -- Get the peripheral
-    local requester = peripheral.wrap(peripheralSide)
-    local items = requester.items()
+    -- Clear the monitor
+    monitor.setBackgroundColor(colors.black)
+    monitor.clear()
 
-    -- Clear the term
-    term.clear()
+    -- Draw grid
+    paintutils.drawBox(1, 1, monitor.getWidth(), monitor.getHeight(), colors.white)
 
-    -- Iterate over items and print them
+    -- Handle each item
     for _, item in ipairs(items) do
         local itemName = generics.shortenName(item.name, 15)
         local itemCount = item.count
+        local change = 0
 
         -- If the item was already present, calculate the change
-        local change = 0
         if prevItems[itemName] then
             change = itemCount - prevItems[itemName]
         end
 
-        -- Format the item and print it
-        local formattedItem = formatItem({
-            name = itemName,
-            sign = change >= 0 and "+" or "-",
-            change = math.abs(change)
-        })
-        print(formattedItem)
-
-        -- Update the previous items
+        -- Save the current count for next iteration
         prevItems[itemName] = itemCount
+
+        -- Display the changes
+        local x = (_ - 1) % 7 * cellWidth + 2
+        local y = math.floor((_ - 1) / 7) * cellHeight + 2
+
+        monitor.setCursorPos(x, y)
+        monitor.setTextColor(colors.white)
+        monitor.write(itemName)
+
+        -- Write change with color
+        monitor.setCursorPos(x, y + 1)
+        if change < 0 then
+            monitor.setTextColor(colors.red)
+            monitor.write("-" .. tostring(math.abs(change)))
+        else
+            monitor.setTextColor(colors.green)
+            monitor.write("+" .. tostring(change))
+        end
     end
 
-    -- Sleep for a bit to prevent constant updates
+    -- Wait before next iteration
     sleep(10)
 end
-
--- Callback for the draw phase
-function cobalt.draw()
-
-end
-
--- Callback for mouse press
-function cobalt.mousepressed(x, y, button)
-
-end
-
--- Callback for mouse release
-function cobalt.mousereleased(x, y, button)
-
-end
-
--- Callback for key press
-function cobalt.keypressed(keycode, key)
-
-end
-
--- Callback for key release
-function cobalt.keyreleased(keycode, key)
-
-end
-
--- Callback for text input
-function cobalt.textinput(t)
-
-end
-
--- Start Cobalt
-cobalt.initLoop()
