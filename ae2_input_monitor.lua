c3fba73b9d8e99b6183b90b0c6d8e76a9eef204f
Joris Schelfhout <joris@shelfwood.co>
2023-07-27 22:58:58 +0200

    GPT

--- ae2_input_monitor.lua
+++ ae2_input_monitor.lua
@@ -9,19 +9,21 @@ monitor.setTextScale(0.7)
 -- Get monitor dimensions
 local monitorWidth, monitorHeight = monitor.getSize()
 
--- Calculate cell dimensions for a 5x7 grid
+-- Calculate cell dimensions for a 7x12 grid
 local cellWidth = monitorWidth / 7
 local cellHeight = monitorHeight / 12
 
 -- Store previous items
 local prevItems = {}
 
+-- Store previous changes and timestamps
+local changes = {}
+
 -- Function to track input of items
 while true do
     -- Get items
     local items = peripheral.wrap(peripheralSide).items()
     local currentItems = {}
-    local changes = {}
 
     -- Calculate changes and sort items
     for _, item in ipairs(items) do
@@ -35,14 +37,13 @@ while true do
         if prevItems[itemName] then
             local change = itemCount - prevItems[itemName]
 
-            -- If there was a change, store it
+            -- If there was a change, store it and update the timestamp
             if change ~= 0 then
                 changes[itemName] = {
                     change = math.abs(change),
-                    sign = change > 0 and "+" or "-"
+                    sign = change > 0 and "+" or "-",
+                    time = os.time()
                 }
-            else
-                changes[itemName] = nil
             end
         end
     end
@@ -50,17 +51,18 @@ while true do
     -- Update the previous items table
     prevItems = currentItems
 
-    -- Convert the changes table to a list and sort it by absolute value of change
+    -- Convert the changes table to a list and sort it by timestamp
     local sortedChanges = {}
     for itemName, changeData in pairs(changes) do
         table.insert(sortedChanges, {
             name = itemName,
-            change = changeData and changeData.change or 0,
-            sign = changeData and changeData.sign or "+"
+            change = changeData.change,
+            sign = changeData.sign,
+            time = changeData.time
         })
     end
     table.sort(sortedChanges, function(a, b)
-        return a.change > b.change
+        return a.time > b.time
     end)
 
     -- Save previous terminal and redirect to monitor
@@ -78,6 +80,7 @@ while true do
         local itemName = item.name
         local change = item.change
         local sign = item.sign
+        local time = item.time
 
         -- Calculate center of each cell for text placement
         local x = (_ - 1) % 7 * cellWidth + math.ceil(cellWidth / 2)
@@ -99,6 +102,13 @@ while true do
         end
         monitor.setBackgroundColor(colors.black) -- Reset the background color before writing
         monitor.write(changeStr)
+
+        -- Write time since change
+        local timeStr = os.time() - time .. " sec. ago"
+        monitor.setCursorPos(x - math.floor(#timeStr / 2), y + 2)
+        monitor.setTextColor(colors.gray)
+        monitor.setBackgroundColor(colors.black) -- Reset the background color before writing
+        monitor.write(timeStr)
     end
 
     -- Restore original terminal
