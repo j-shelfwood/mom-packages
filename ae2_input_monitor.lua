b7368f0e97d80c02d57dfb088cd69e0f837b9588
Joris Schelfhout <joris@shelfwood.co>
2023-07-27 20:56:48 +0200

    GPT

--- ae2_input_monitor.lua
+++ ae2_input_monitor.lua
@@ -1,64 +1,9 @@
-local cobalt = require("cobalt")
+local cobalt = dofile("path/to/cobalt") -- replace with the correct path to your Cobalt file
 local generics = require("generics")
-local peripheralSide = generics.findPeripheralSide("merequester:requester")
-
--- Define the format for our items.
-local function formatItem(item)
-    return item.name .. "\n" .. item.sign .. " " .. tostring(item.change)
-end
-
--- Define our main loop
-cobalt.loop = function()
-    -- Get our peripheral
-    local requester = peripheral.wrap(peripheralSide)
-    local items = requester.items()
-    local changes = {}
-
-    -- Clear out our view
-    cobalt.view.children = {}
-
-    -- Create a row for each item
-    for _, item in ipairs(items) do
-        local itemName = generics.shortenName(item.name, 15)
-        local itemCount = item.count
-
-        -- If the item was already present, calculate the change
-        if prevItems and prevItems[itemName] then
-            local change = itemCount - prevItems[itemName]
-            -- If there was a change, store it
-            if change ~= 0 then
-                changes[itemName] = {
-                    change = math.abs(change),
-                    sign = change > 0 and "+" or "-"
-                }
-            else
-                changes[itemName] = nil
-            end
-        end
-
-        -- Add our item to the view
-        local column = cobalt.ui.new("Column", {
-            width = 15,
-            height = 3
-        })
-        local label = cobalt.ui.new("Label", {
-            text = formatItem({
-                name = itemName,
-                sign = changes[itemName] and changes[itemName].sign or "+",
-                change = changes[itemName] and changes[itemName].change or 0
-            })
-        })
-
-        column:addChild(label)
-        cobalt.view:addChild(column)
-    end
-
-    -- Store the current items for the next iteration
-    prevItems = items
-end
 
 -- Automatically find the sides
 local monitorSide = generics.findPeripheralSide("monitor")
+local peripheralSide = generics.findPeripheralSide("merequester:requester")
 
 if not monitorSide then
     print("Monitor not found.")
@@ -70,6 +15,79 @@ if not peripheralSide then
     return
 end
 
+-- Global variable to store the previous items
+prevItems = {}
+
+-- Helper function to format an item
+function formatItem(item)
+    return item.name .. "\n" .. item.sign .. " " .. tostring(item.change)
+end
+
+-- Callback for the update phase
+function cobalt.update(dt)
+    -- Get the peripheral
+    local requester = peripheral.wrap(peripheralSide)
+    local items = requester.items()
+
+    -- Clear the term
+    term.clear()
+
+    -- Iterate over items and print them
+    for _, item in ipairs(items) do
+        local itemName = generics.shortenName(item.name, 15)
+        local itemCount = item.count
+
+        -- If the item was already present, calculate the change
+        local change = 0
+        if prevItems[itemName] then
+            change = itemCount - prevItems[itemName]
+        end
+
+        -- Format the item and print it
+        local formattedItem = formatItem({
+            name = itemName,
+            sign = change >= 0 and "+" or "-",
+            change = math.abs(change)
+        })
+        print(formattedItem)
+
+        -- Update the previous items
+        prevItems[itemName] = itemCount
+    end
+
+    -- Sleep for a bit to prevent constant updates
+    sleep(10)
+end
+
+-- Callback for the draw phase
+function cobalt.draw()
+
+end
+
+-- Callback for mouse press
+function cobalt.mousepressed(x, y, button)
+
+end
+
+-- Callback for mouse release
+function cobalt.mousereleased(x, y, button)
+
+end
+
+-- Callback for key press
+function cobalt.keypressed(keycode, key)
+
+end
+
+-- Callback for key release
+function cobalt.keyreleased(keycode, key)
+
+end
+
+-- Callback for text input
+function cobalt.textinput(t)
+
+end
+
 -- Start Cobalt
-cobalt.init(monitorSide)
-cobalt.run()
+cobalt.initLoop()
