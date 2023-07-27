5a6d3f6e1fab29b2c5a880bff4e94ea767a28270
Joris Schelfhout <joris@shelfwood.co>
2023-07-27 20:05:06 +0200

    GPT

--- ae2_input_monitor.lua
+++ ae2_input_monitor.lua
@@ -1,3 +1,8 @@
+local generics = require("generics")
+local GridMonitor = require("grid_monitor") -- require the GridMonitor module
+
+local scale = tonumber(arg[1]) or 1
+
 -- Function to track input of items
 function trackInput(monitorSide, peripheralSide)
     -- Get a reference to the monitor and the peripheral
@@ -70,3 +75,20 @@ function trackInput(monitorSide, peripheralSide)
         sleep(30)
     end
 end
+
+-- Automatically find the sides
+local monitorSide = generics.findPeripheralSide("monitor")
+local peripheralSide = generics.findPeripheralSide("merequester:requester")
+
+if not monitorSide then
+    print("Monitor not found.")
+    return
+end
+
+if not peripheralSide then
+    print("ME Requester not found.")
+    return
+end
+
+-- Call the function to track the input of items
+trackInput(monitorSide, peripheralSide)
