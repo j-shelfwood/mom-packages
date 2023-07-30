af6fcf651a67d3a803edaa17a6532df4b6c33e4e
Joris Schelfhout <joris@shelfwood.co>
2023-07-30 20:22:08 +0200

    Again

--- enslavement.lua
+++ enslavement.lua
@@ -1,8 +1,17 @@
 -- enslavement.lua
--- Open the modem for Rednet communication
+function findPeripheralSide(name)
+    local sides = {"top", "bottom", "left", "right", "front", "back"}
+    for _, side in ipairs(sides) do
+        if peripheral.isPresent(side) and peripheral.getType(side) == name then
+            return side
+        end
+    end
+    return nil
+end
+
 -- Automatically detect which side has the modem 
--- and open it for Rednet communication (without using generics)
-rednet.open(peripheral.find("modem").getName())
+local modemSide = findPeripheralSide("modem")
+rednet.open(modemSide)
 
 -- Wait for a Rednet message
 local senderID, message, protocol = rednet.receive()
