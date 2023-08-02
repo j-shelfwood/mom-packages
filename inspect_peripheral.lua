b621644173e7a412a44674b338f469c32daa2a00
Joris Schelfhout <joris@shelfwood.co>
2023-08-02 20:30:11 +0200

    Update inspect peripheral command

--- inspect_peripheral.lua
+++ inspect_peripheral.lua
@@ -2,30 +2,31 @@
 function inspectPeripheral(side)
     -- Check if there is a peripheral connected on the given side
     if not peripheral.isPresent(side) then
-      print("No peripheral present on the " .. side .. " side.")
-      return
+        print("No peripheral present on the " .. side .. " side.")
+        return
     end
-    
+
     -- Get the methods of the peripheral
     local methods = peripheral.getMethods(side)
-  
+
     -- If there are no methods available, print a message and return
     if methods == nil or #methods == 0 then
-      print("No methods available for the peripheral on the " .. side .. " side.")
-      return
+        print("No methods available for the peripheral on the " .. side .. " side.")
+        return
     end
-  
+
     -- Print the methods
     print("Methods for the peripheral on the " .. side .. " side:")
     for _, method in ipairs(methods) do
-      print(method)
+        print(method)
     end
-  end
-  
-  -- Ask the user to enter the side
-  print("Enter the side where the peripheral is connected (top, bottom, left, right, front or back):")
-  local side = read()
-  
-  -- Call the function to inspect the peripheral
-  inspectPeripheral(side)
-  
+end
+
+-- Auto-detect any peripherals connected to the computer
+local sides = {"top", "bottom", "left", "right", "front", "back"}
+for _, side in ipairs(sides) do
+    inspectPeripheral(side)
+end
+
+-- Call the function to inspect the peripheral
+inspectPeripheral(side)
