4653f415a6949da60c0f5bbaef89d8bf2bd3ca24
Joris Schelfhout <joris@shelfwood.co>
2023-08-13 19:32:05 +0200

    Fix it

--- machine_status_display.lua
+++ machine_status_display.lua
@@ -1,6 +1,5 @@
 -- Include Data Processing and Grid Display APIs
 local wpp = require('wpp')
-local GridDisplay = require('grid_display')
 local generics = require('generics')
 
 -- Connect to the wireless peripheral network
@@ -8,73 +7,73 @@ wpp.wireless.connect("shelfwood")
 
 -- Wrap the monitor
 local monitor = peripheral.wrap(generics.findPeripheralSide('monitor'))
-local display = GridDisplay.new(monitor)
+local width, height = monitor.getSize()
+print("Monitor resolution:", width, "x", height) -- Debug output for monitor resolution
 
--- Define a formatting callback for the grid display
-local function format_callback(item)
-    -- Format the machine data
-    local progressPercentage = string.format("%.1f%%", item.progress * 100) -- Convert the float to a percentage with 1 decimal point
-    local efficiencyInfo = tostring(item.currentEfficiency)
-    local craftingInfo = "-" -- Default value
-
-    if item.items and #item.items > 0 then -- Assuming the items method returns a list
-        craftingInfo = item.items[1].count .. 'x ' .. item.items[1].displayName -- Display the first item
-        print(craftingInfo)
-        print(textutils.serialize(item.items))
-    end
-
-    return {
-        line_1 = (tostring(item.energy) .. "/" .. tostring(item.capacity)) or "N/A",
-        color_1 = colors.blue,
-        line_2 = progressPercentage .. " | " .. efficiencyInfo,
-        color_2 = colors.white,
-        line_3 = craftingInfo,
-        color_3 = item.isBusy and colors.green or colors.blue
-    }
+if width ~= 18 or height < 69 then
+    print("Invalid monitor size! Expected: 18x69 or larger")
+    return
 end
+
+monitor.setTextScale(1)
 
 -- Function to fetch machine data
 local function fetch_data(machine_type)
     local machine_data = {}
     local peripherals = wpp.peripheral.getNames()
-    -- Display count of peripherals
-    print("Found " .. #peripherals .. " peripherals on the network.")
-
     for _, name in ipairs(peripherals) do
         local machine = wpp.peripheral.wrap(name)
-
         -- Filter by the given machine type
         if string.find(name, machine_type) then
-            print("Fetching data for " .. name)
-            machine.wppPrefetch({"getEnergy", "isBusy", "getEnergyCapacity", "getCraftingInformation", "items"})
-
-            -- Extract the name
-            local _, _, name = string.find(name, machine_type .. "_(.+)")
-
-            local craftingInfo = machine.getCraftingInformation() or {}
-            local itemsList = machine.items() or {}
-
+            machine.wppPrefetch({"isBusy"})
+            -- Extract the machine number
+            local _, _, number = string.find(name, "_(%d+)$")
             table.insert(machine_data, {
-                name = name,
-                energy = machine.getEnergy(),
-                capacity = machine.getEnergyCapacity(),
-                progress = craftingInfo.progress or 0,
-                currentEfficiency = craftingInfo.currentEfficiency or 0,
-                items = itemsList,
+                number = number,
                 isBusy = machine.isBusy()
             })
         end
     end
-
+    -- Sort the machine data by machine number
+    table.sort(machine_data, function(a, b)
+        return tonumber(a.number) < tonumber(b.number)
+    end)
     return machine_data
 end
 
--- Function to refresh the display
-local function refresh_display(machine_type)
+-- Function to display machine status visually
+local function display_machine_status(machine_type)
     local machine_data = fetch_data(machine_type)
-
-    -- Display the machine data on the monitor
-    display:display(machine_data, format_callback)
+    print("Found", #machine_data, "machines")
+    if #machine_data == 0 then
+        return
+    end
+    monitor.clear()
+    local bar_width = (width - 3) / 2 -- Half the width, minus 1 for border and 1 for space between bars
+    local bar_height = (height - 12) / 12 -- Account for 12 bars and 11 spaces
+    for idx, machine in ipairs(machine_data) do
+        local column = (idx - 1) % 2
+        local row = math.ceil(idx / 2)
+        local x = column * (bar_width + 1) + 2 -- +2 to account for left border and space between bars
+        local y = (row - 1) * (bar_height + 1) + 2 -- +2 to account for top border and space between bars
+        -- Draw a colored bar based on isBusy status
+        if machine.isBusy then
+            monitor.setBackgroundColor(colors.green)
+        else
+            monitor.setBackgroundColor(colors.gray)
+        end
+        for i = 0, bar_height - 1 do
+            monitor.setCursorPos(x, y + i)
+            monitor.write(string.rep(" ", bar_width))
+        end
+        -- Write the machine number centered in the bar
+        monitor.setTextColor(colors.black)
+        monitor.setCursorPos(x + math.floor((bar_width - string.len(machine.number)) / 2),
+            y + math.floor(bar_height / 2))
+        monitor.write(machine.number)
+    end
+    monitor.setBackgroundColor(colors.black) -- Reset background color
+    monitor.setTextColor(colors.white) -- Reset text color
 end
 
 -- Get machine type from command line parameter
@@ -87,8 +86,8 @@ if not machine_type then
     return
 end
 
--- Run the refresh_display function every 15 seconds
+-- Run the display_machine_status function every 5 seconds
 while true do
-    refresh_display(machine_type)
-    os.sleep(15)
+    display_machine_status(machine_type)
+    os.sleep(5)
 end
