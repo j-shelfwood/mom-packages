1d7169a8c225bd9f9c1d1cd6046edbe85e70fe0f
Joris Schelfhout <joris@shelfwood.co>
2023-08-13 03:14:33 +0200

    Add machine status script

---
+++ machine_status_display.lua
@@ -0,0 +1,68 @@
+-- Include Data Processing and Grid Display APIs
+local wpp = require('wpp')
+local GridDisplay = require('grid_display')
+local generics = require('generics')
+
+-- Connect to the wireless peripheral network
+wpp.wireless.connect("shelfwood")
+
+-- Wrap the monitor
+local monitor = peripheral.wrap(generics.findPeripheralSide('monitor'))
+local display = GridDisplay.new(monitor)
+
+-- Define a formatting callback for the grid display
+local function format_callback(item)
+    -- Format the machine data
+    return {
+        line_1 = item.name,
+        color_1 = colors.white,
+        line_2 = item.isBusy and "Busy" or "Idle",
+        color_2 = item.isBusy and colors.red or colors.green,
+        line_3 = "E: " .. tostring(item.energy) .. "/" .. tostring(item.capacity),
+        color_3 = colors.blue
+    }
+end
+
+-- Function to fetch machine data
+local function fetch_data(machine_type)
+    local machine_data = {}
+    local peripherals = wpp.peripheral.getNames()
+
+    for _, name in ipairs(peripherals) do
+        local machine = wpp.peripheral.wrap(name)
+
+        -- Filter by the given machine type
+        if string.find(name, machine_type) then
+            machine.wppPrefetch({"getEnergy", "isBusy", "getEnergyCapacity"})
+
+            -- Extract the name
+            local _, _, name = string.find(name, machine_type .. "_(.+)")
+
+            table.insert(machine_data, {
+                name = name,
+                energy = machine.getEnergy(),
+                isBusy = machine.isBusy(),
+                capacity = machine.getEnergyCapacity()
+            })
+        end
+    end
+
+    return machine_data
+end
+
+-- Function to refresh the display
+local function refresh_display(machine_type)
+    local machine_data = fetch_data(machine_type)
+
+    -- Display the machine data on the monitor
+    display:display(machine_data, format_callback)
+end
+
+-- Choose the machine type to display
+local machine_type = "electrolyzer" -- Change this to your desired machine type
+
+-- Run the refresh_display function every 15 seconds
+while true do
+    refresh_display(machine_type)
+    os.sleep(15)
+end
