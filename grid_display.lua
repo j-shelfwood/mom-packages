29c6c127e44e8512f80f28134378c41905c07574
Joris Schelfhout <joris@shelfwood.co>
2023-08-16 22:38:34 +0200

    Debug output

--- grid_display.lua
+++ grid_display.lua
@@ -47,17 +47,16 @@ function GridDisplay:calculate_cells(num_items)
 
         if required_rows <= max_rows_at_current_height then
             self:setCellParameters(num_items, width, height, max_columns, required_rows, scale)
+            print("Scale:", scale) -- Debugging output
+            print("Monitor Width:", width, "Monitor Height:", height) -- Debugging output
+            print("Max Columns:", max_columns) -- Debugging output
+            print("Required Rows:", required_rows) -- Debugging output
 
             self.rows = required_rows
             return
         end
         scale = scale - SCALE_DECREMENT
     end
-
-    print("Scale:", scale) -- Debugging output
-    print("Monitor Width:", width, "Monitor Height:", height) -- Debugging output
-    print("Max Columns:", max_columns) -- Debugging output
-    print("Required Rows:", required_rows) -- Debugging output
 
     -- Minimum scale settings
     self.scale = MIN_TEXT_SCALE
