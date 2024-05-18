7b7bfcc7c2ee088802c56f2b793402b92478b746
Joris Schelfhout <joris@shelfwood.co>
2024-05-18 12:12:59 +0200

    Add advanced_chest_monitor to filelist.lua

--- views/filelist.lua
+++ views/filelist.lua
@@ -1,3 +1,3 @@
 -- filelist for views module
-return {"advanced_item_monitor", "item_monitor", "machine_activity_display", "machine_status_display", "hello",
+return {"advanced_chest_monitor", "advanced_item_monitor", "item_monitor", "machine_activity_display", "machine_status_display", "hello",
         "fluid_monitor", "storage_dashboard", "inventory_changes", "powah_energy_display"}
